22ff473054601e6fce945e9a21039a249c2e531b
Eren Tasci <erent4sci@gmail.com>
2022-11-28 13:39:04 +0300

    changed page files

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,11 +1,83 @@
 import React from "react";
+import Image from "next/image";
+import styles from "./contact.module.scss";
 
-const about = () => {
+const contact = () => {
   return (
-    <div>
-      <h1>About Us</h1>
-    </div>
+    <>
+      <div className={styles["about"]}>
+        <div className={styles["container"]}>
+          <div className={styles["about-container"]}>
+            <div className={styles["about-left"]}>
+              <Image
+                src="/images/aboutus.png"
+                alt="Contact us picture"
+                width={400}
+                height={300}
+              />
+            </div>
+            <div className={styles["about-right"]}>
+              <h2>Know About Our Ecommerce Business, History</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mattis
+                neque ultrices mattis aliquam, malesuada diam est. Malesuada sem
+                tristique amet erat vitae eget dolor lobortis. Accumsan faucibus
+                vitae lobortis quis bibendum quam.
+              </p>
+              <button>About us</button>
+            </div>
+          </div>
+
+          <div className={styles["features"]}>
+            <h3>Our Features</h3>
+            <div className={styles["features-cards"]}>
+              <div className={styles["features-card"]}>
+                <Image src="/images/free-delivery.png" width={65} height={65} />
+
+                <h5>Free Delivery</h5>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa
+                  purus gravida.
+                </p>
+              </div>
+              <div className={styles["features-card"]}>
+                <Image src="/images/cashback.png" width={65} height={65} />
+                <h5>100% Cash Back</h5>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa
+                  purus gravida.
+                </p>
+              </div>
+              <div className={styles["features-card"]}>
+                <Image
+                  src="/images/premium-quality.png"
+                  width={65}
+                  height={65}
+                />
+                <h5>Quality Product</h5>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa
+                  purus gravida.
+                </p>
+              </div>
+              <div className={styles["features-card"]}>
+                <Image
+                  src="/images/24-hours-support.png"
+                  width={65}
+                  height={65}
+                />
+                <h5>24/7 Support</h5>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa
+                  purus gravida.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
-export default about;
+export default contact;
--- pages/products/index.js
+++ pages/products/index.js
@@ -1,8 +1,25 @@
 import React from "react";
 
-const products = () => {
-  return <h1>Products</h1>;
+const products = ({ products }) => {
+  console.log(products);
+  return (
+    <>
+      {products.map((product) => (
+        <h2>{product.title}</h2>
+      ))}
+    </>
+  );
 };
 
+export async function getServerSideProps(context) {
+  const req = await fetch("https://fakestoreapi.com/products");
+  const products = await req.json();
+
+  return {
+    props: {
+      products,
+    }, // will be passed to the page component as props
+  };
+}
 
 export default products;
